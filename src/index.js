1a346edcb75a0e0cfa6bd98ab4ce6f6c9d98c959
rufunt <jazzoo@mail.ru>
2019-05-23 13:13:26 +0300

    Init commit and create simple components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const TodoList = () => {
+  return (
+    <ul>
+      <li>ToDo 1</li>
+      <li>todo 2</li>
+    </ul>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const AppHeader = () => {
+  return <h1>Todo Items</h1>;
+};
+
+const SearchPanel = () => {
+  return (
+    <input placeholder='search' />
+  );
+};
+
+const el = (
+  <div>
+    <AppHeader />
+    <SearchPanel />
+    <TodoList /> 
+  </div>
+);
+
+ReactDom.render(el, document.getElementById('root'));
