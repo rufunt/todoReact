25dda12857e129cdcb6647103b14ee7817e01860
rufunt <jazzoo@mail.ru>
2019-05-26 14:40:57 +0300

    Update Components as Classes

--- src/components/app-header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './app-header.css';
-
-const AppHeader = () => {
-  return <h1>Todo Items</h1>;
-};
-
-export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-import TodoList from './components/todo-list';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import ItemStatusFilter from './components/item-status-filter';
+import App from './components/app';
 
-import './index.css';
-
-
-const App = () => {
-
-  const todoData = [
-    { label: "Todo 1", important: false, id: 1 },
-    { label: "Todo 2", important: true, id: 2 },
-    { label: "Todo 3", important: false, id: 3 }
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList todos={todoData} />
-</div>
-  );
-};
-
-
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+document.getElementById('root'));
